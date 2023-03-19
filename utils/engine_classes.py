b209a0f5d9430f98d1759925e39574c1c7e37095
GeorgijP <georgij.puzanov@inbox.ru>
2023-03-19 15:22:37 +0400

    Написал логику для получения вакансий с HH и SJ + запись полученных данных в файлы .json

--- utils/engine_classes.py
+++ utils/engine_classes.py
@@ -1,5 +1,8 @@
+import json
+
 import requests
 from abc import ABC, abstractmethod
+
 
 class Engine(ABC):
     @abstractmethod
@@ -14,15 +17,51 @@ class Engine(ABC):
 
 class HH(Engine):
     def get_request(self):
-        url = "https://api.hh.ru/vacancies/"
-        par = {'text': 'python', 'area': 113}
-        self.request = requests.get(url, params=par).json()
-        return self.request
+        items = []
+
+        for i in range(50):
+            url = "https://api.hh.ru/vacancies"
+            par = {
+                'text': 'python',
+                'areas': 113,
+                'page': i
+            }
+            self.request = requests.get(url, params=par).json()
+            if self.request == []:
+                continue
+            items.append(self.request['items'])
+
+        with open('vacanciesHH.json', 'w') as f:
+            json.dump(items, f, indent=4)
 
 
 class Superjob(Engine):
     def get_request(self):
-        url = 'https://api.superjob.ru/2.0/vacancies/'
-        headers = {"X-Api-App-Id": "v3.r.137427491.f38a88a81cfa9ab6d514bb52ef8d8d9d17c470cb.30e3a8df42b0b4854ccd429931ac7d3a7b8e3c34"}
-        self.request = requests.get(url, headers=headers).json()
-        return self.request
+        items = []
+
+        for i in range(5):
+            url = 'https://api.superjob.ru/2.0/vacancies/'
+            headers = {
+                "X-Api-App-Id": "v3.r.137427491.f38a88a81cfa9ab6d514bb52ef8d8d9d17c470cb.30e3a8df42b0b4854ccd429931ac7d3a7b8e3c34"
+            }
+            par = {
+                'keywords': 'python',
+                'page': i,
+                'count': 100
+            }
+            self.request = requests.get(url, headers=headers, params=par).json()
+            if self.request == []:
+                continue
+            items.append(self.request['objects'])
+
+        with open('vacanciesSJ.json', 'w') as f:
+            json.dump(items, f, indent=4)
+
+
+hh = HH()
+print(hh.get_request())
+# print(len(hh.get_request()))
+
+sj = Superjob()
+print(sj.get_request())
+# print(len(sj.get_request()))

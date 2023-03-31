2d641e089286335212a21113db34efae63932123
GeorgijP <georgij.puzanov@inbox.ru>
2023-03-31 19:37:01 +0400

    Добавил описания

--- utils/engine_classes.py
+++ utils/engine_classes.py
@@ -1,66 +1,66 @@
-import json
-
 import requests
+import os
+import jmespath
 from abc import ABC, abstractmethod
 
+
 class Engine(ABC):
+
     @abstractmethod
     def get_request(self):
         ...
 
-    @staticmethod
-    def get_connector(file_name):
-        """ Возвращает экземпляр класса Connector """
-        pass
-
 
 class HH(Engine):
+    """
+    Делает GET запрос к api hh.ru
+    """
+    def __init__(self, keyword):
+        self.keyword = keyword
+
     def get_request(self):
         items = []
 
-        for i in range(1):
+        for i in range(10):
             url = "https://api.hh.ru/vacancies"
             par = {
-                'text': 'python',
+                'text': self.keyword,
                 'areas': 113,
-                'page': i
+                'page': i,
+                'per_page': 100,
             }
-            self.request = requests.get(url, params=par).json()
-            if self.request == []:
+            self.response = requests.get(url, params=par).json()
+            if self.response == []:
                 continue
-            for y in range(20):
-                items.append(self.request['items'][y]['name'])
-                items.append(self.request['items'][y]['alternate_url'])
-                items.append(self.request['items'][y]['snippet']['requirement'])
-                items.append(self.request['items'][y]['salary'])
+            items.append(jmespath.search('items[*]', self.response)) # Такая конструкция быстрее обрабатывает данные
 
-        with open('vacanciesHH.json', 'w') as f:
-            json.dump(items, f, indent=4)
+        return items
 
 
 class Superjob(Engine):
+    """
+    Делает GET запрос к api Superjob.ru
+    """
+    def __init__(self, keyword):
+        self.keyword = keyword
+        self.api_key = os.environ.get('SECRET_KEY_SJ')
+
     def get_request(self):
         items = []
 
-        for i in range(1):
+        for i in range(10):
             url = 'https://api.superjob.ru/2.0/vacancies/'
             headers = {
-                "X-Api-App-Id": "v3.r.137427491.f38a88a81cfa9ab6d514bb52ef8d8d9d17c470cb.30e3a8df42b0b4854ccd429931ac7d3a7b8e3c34"
+                "X-Api-App-Id": self.api_key
             }
             par = {
-                'keywords': 'python',
+                'keywords': self.keyword,
                 'page': i,
                 'count': 100
             }
-            self.request = requests.get(url, headers=headers, params=par).json()
-            if self.request == []:
+            self.response = requests.get(url, headers=headers, params=par).json()
+            if self.response == []:
                 continue
-            for y in range(100):
-                items.append(self.request['objects'][y]['profession'])
-                items.append(self.request['objects'][y]['link'])
-                items.append(self.request['objects'][y]['candidat'])
-                items.append(self.request['objects'][y]['payment_from'])
+            items.append(jmespath.search('objects[*]', self.response)) # Такая конструкция быстрее обрабатывает данные
 
-        with open('vacanciesSJ.json', 'w') as f:
-            json.dump(items, f, indent=4)
-
+        return items
--- utils/jobs_classes.py
+++ utils/jobs_classes.py
@@ -1,4 +1,7 @@
 class Vacancy:
+    """
+    Делает данные читабильными
+    """
     __slots__ = ('job_title', 'job_link', 'job_description', 'job_salary')
 
     def __init__(self, job_title, job_link, job_description, job_salary):
@@ -10,42 +13,3 @@ class Vacancy:
 
     def __str__(self):
         return f"Название вакансии - {self.job_title}\nОписание: {self.job_description}\nЗарплата: {self.job_salary}\nСсылка на вакансию{self.job_link}"
-
-
-
-class CountMixin:
-
-    @property
-    def get_count_of_vacancy(self):
-        """
-        Вернуть количество вакансий от текущего сервиса.
-        Получать количество необходимо динамически из файла.
-        """
-        pass
-
-
-
-class HHVacancy(Vacancy):  # add counter mixin
-    """ HeadHunter Vacancy """
-
-
-    def __str__(self):
-        return f'HH: {self.job_title}, зарплата: {self.job_salary} руб/мес'
-
-
-
-class SJVacancy(Vacancy):  # add counter mixin
-    """ SuperJob Vacancy """
-
-    def __str__(self):
-        return f'SJ: {self.job_title}, зарплата: {self.job_salary} руб/мес'
-
-
-def sorting(vacancies):
-    """ Должен сортировать любой список вакансий по ежемесячной оплате (gt, lt magic methods) """
-    pass
-
-
-def get_top(vacancies, top_count):
-    """ Должен возвращать {top_count} записей из вакансий по зарплате (iter, next magic methods) """
-    pass
